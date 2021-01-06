91ee37bef2206e7443a76772b7467e94b77eebdc
Damian Krzeminski <krzemdamian@gmail.com>
2021-01-06 22:26:53 +0100

    Remove if __main__

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ from server import Server
 HOST_NAME = '0.0.0.0'
 PORT_NUMBER = 8000
 
-if __name__ == '__main__':
-    httpd = HTTPServer((HOST_NAME, PORT_NUMBER), Server)
-    print(time.asctime(), 'Server UP - %s:%s' % (HOST_NAME, PORT_NUMBER))
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    print(time.asctime(), 'Server DOWN - %s:%s' % (HOST_NAME, PORT_NUMBER))
+httpd = HTTPServer((HOST_NAME, PORT_NUMBER), Server)
+print(time.asctime(), 'Server UP - %s:%s' % (HOST_NAME, PORT_NUMBER))
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    pass
+httpd.server_close()
+print(time.asctime(), 'Server DOWN - %s:%s' % (HOST_NAME, PORT_NUMBER))
